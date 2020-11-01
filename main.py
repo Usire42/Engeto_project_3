578c7b9b14fd6ff55ae0fbf608a8170650044d45
Fedorko <j.fedorko.usire@gmail.com>
2020-11-01 20:53:10 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from functions import inf_URL_to_csv
+
+def main(URL,PATHWAY):
+    inf_URL_to_csv(URL, PATHWAY)
+
+
+if __name__ == '__main__':
+    URL = 'https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2111'
+    PATHWAY = 'C:/Users/User/PycharmProjects/projects/'  '''wtire pathvay to file example:  
+                                                            "'C:/Users/User/PycharmProjects/projects/" '''
+    main(URL, PATHWAY)
+
+
